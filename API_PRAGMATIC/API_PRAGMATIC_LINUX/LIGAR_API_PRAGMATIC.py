b31f8734a6a85de8a5e2fc93539024b5aa5d9f59
Francilino Neto <88691125+Francilino-Netoo@users.noreply.github.com>
2024-07-01 07:21:52 -0300

    Add files via upload

---
+++ API_PRAGMATIC/API_PRAGMATIC_LINUX/LIGAR_API_PRAGMATIC.py
@@ -0,0 +1,13 @@
+import time 
+import subprocess
+while True:
+    
+    try:
+        returned_value =subprocess.Popen(["./API_PRAGMATIC"])
+        returned_value.wait()
+
+    except:
+        time.sleep(3)
+        print('oo')
+        
+        pass
